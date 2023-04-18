f80d1c1096152e3fe5f4d2a52ff2523e1797d9e4
Yoel Fernandez Suarez <yoel.fernandez@izertis.com>
2023-04-18 13:07:02 +0200

    finalizacion

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent,
+    LoadingSpinnerComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent,
+    LoadingSpinnerComponent,
+
+  ]
+})
+export class SharedModule { }
